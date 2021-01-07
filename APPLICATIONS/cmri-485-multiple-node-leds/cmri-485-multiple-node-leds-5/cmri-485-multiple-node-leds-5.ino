1241e2b02d45cb3c1acd967afdb1a48e493985fd
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-01-07 15:55:53 -0600

    adding image

---
+++ APPLICATIONS/cmri-485-multiple-node-leds/cmri-485-multiple-node-leds-5/cmri-485-multiple-node-leds-5.ino
@@ -0,0 +1,24 @@
+#include <CMRI.h>
+#include <Auto485.h>
+
+#define CMRI_ADDR 5
+
+#define LED 13
+
+#define DE_PIN 2
+
+Auto485 bus(DE_PIN); 
+
+CMRI cmri(CMRI_ADDR, 24, 48, bus);
+
+void setup() {
+     pinMode(LED, OUTPUT);
+     bus.begin(9600);
+}
+
+void loop() {
+
+     cmri.process();
+     digitalWrite(LED, cmri.get_bit(0));
+
+}
