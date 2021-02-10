29f7c6fa5cb30e1d685cf401822a13709f3c2526
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-09 21:41:42 -0600

    cmri-rgb-multiblock-singal-system

---
+++ APPLICATIONS/cmri-rgb-multiblock-singal-system/README.md
@@ -0,0 +1,133 @@
+# RGB Multblock decection system 
+
+--- 
+
+![img](https://github.com/adarshkumarsingh83/jmri-cmri/blob/main/APPLICATIONS/cmri-rgb-multiblock-singal-system/CONNECTIONS.JPG)
+
+## Connection Deatils 
+
+```
+  
+Assumption: 
+Train can travel from both direction of the block 
+every block start will have IR sensor and end will also have IR sensor 
+train has to click both 1st and 2nd IR sensor one by one either of the direction 
+otherwise it will not show as UnOccupoied even train left the block 
+
+1.connection the +5v and gnd to the breadboard 
+2.connection led1 and led2 to the breadboard gnd long leg (common cathod rgb led) 
+3. then connection 2 resistor to the red and green led of the rgb led for both led1 and led2 
+4. then connect the green long leg of led1 to the 3 pin in arduino  
+5. then connection the red long  leg led11 to the 4 pin in arduino  
+6. then connect the green long leg of led2 to the 5 pin in arduino  
+7. then connect the red long  leg led2 to the 6 pin in arduino  
+8. then connect the IR sensor 1 and 2 to the bread board 
+9. then connect the vcc and gnd to the IR sensor from breadboard +5 and gnd 
+10 then connect the IR sensor1 out to the A0 pin in arduino 
+11 then connect the IR sensor2 out to the A1 pin in arduino 
+12. then connect pin 6 on arduino to the breadboard column which will go the the previous and next block for occupied signal 
+13. take one connection to the previous block arduino and connect to the A3 pin of that 
+14. take one connection to the next block arduion and connect to the A2 pin of that 
+15. take previous block incomming occupied singal connection to the pin A2 of the arduino 
+16. take next block incomming occupied sinal cconnection to the pin A3 of the arduino 
+17. take the connection from gnd pin from arudino to the next and previous block gnd 
+	pins connect all gnd to tmake common ground bus for entre block system. 
+
+```
+
+## CMRI CONFIGURATION IN PANEL PRO
+```
+JMRI PanelPro. When it is running click on edit preferences to get the following window
+
+set the following tabs
+
+System manufacturer = C/MRI
+
+System connection = Serial
+
+Setting
+
+Serial port: (select the port your arduino is connected to, you will find this on the arduino IDE)
+
+Connection Prefix: C
+
+Connection Name: (what ever you want to call it)
+
+Also click on additional connection box and select the Baud rate to 9600 bps
+
+```
+
+### configure the nodes so click on "Configure C/MRI nodes"
+```
+click on add node 
+provide the node address bases on arduino stetch 
+Example below 
+ #define CMRI_ADDR 1
+ #define CMRI_ADDR 2
+ #define CMRI_ADDR 3
+
+in Node Address (UA) textbox 
+Node Type: SMINI 
+
+ after providing above information click on "addd node" and then click on "done"
+ then click on save and restart the panel pro 
+
+```
+
+## Configure the Sensor
+
+```
+
+Open Panel pro 
+tools -> table -> sensors ->  
+click on "add" sensor button 
+   Systemn Connection : C/MRI 
+   Hardware Address : 1001 // based on Arduino sketch 
+   User Name : provide any information 
+
+   Click on "Create" Button 
+
+
+   click on sensors window on top menu Defaults -> Initial Sensor state -> provide inactive in open popup window and -> click on "ok" button 
+
+
+
+NOTE : In Arduino code address for the sensor will start from 0 but in jmri it will start from 1 
+Example: 
+cmri.set_bit(0, xxx);
+1st param is sensor address and 2nd is the value we cant to send to jmri 
+but while configuringg the sensor will start the address by 1001 in which 1000 is arduino address and 1 is the sensor address
+
+
+#NOTE we have to do it for 3 time for 3 blocks 
+
+## for Nodes address in Jmri 
+
+## for Node 1 address is 1000
+sensor address will be 1001
+
+## for Node 2 address is 2000
+sensor address will be 2001
+
+## for Node 3 address is 3000
+sensor address will be 3001
+
+```
+
+
+## Configure in Layout Editor
+```
+Click on Panel Pro window then top menu -> Panel -> New Panel -> Layout Editor 
+
+Click on "Anchor Point" Radio Button then press "Shit key" than do click on the editor we can see the start point is visible on the editor 
+do the same for end point of the track 
+
+Click on the "Track Segment" radio button then check "Mainline" and "Dashed" check button then  Block Name select the sensor name which we created earlier 
+click on "Highlight" Checkbox and "Block Sensor" Name select from drop down sensor which we have created earlier 
+then keep pressing "shift key" and select the point start and drag to the point end track light will apper 
+
+then test the sensor by moving hand on the top of them and in editor track will turn into red 
+
+then save the editor by CRT+ S and provide the name to that and select the location where we want to save the layout information 
+
+```
---
+++ APPLICATIONS/cmri-rgb-multiblock-singal-system/block-3-system/block-3-system.ino
@@ -0,0 +1,343 @@
+#include <CMRI.h>
+#include <Auto485.h>
+
+// change the address no for block no xxx
+#define CMRI_ADDR 3
+#define DE_PIN 2
+
+#define sens1 A0 //Sensor 1 is setup on pin A0
+#define sens2 A1 //Sensor 2 is setup on pin A1
+#define  G1_LED 3 // Green Signal 1 is digital pin 3
+#define  R1_LED 4// Red Signal 1 is digital pin 4
+#define  G2_LED 5// Green Signal 2 is digital pin 5
+#define  R2_LED 6 //Red Signal 2 is digital pin 6
+#define  blockOccSignal 7 // The transmit pin for the previous and next block in the sequence
+#define  prevBlockRec A2// The recieve pin for the previous block in the sequence
+#define  nxtBlockRec A3// The recieve pin for the next block in the sequence
+#define DELAY_VALUE 200
+#define ON  HIGH
+#define OFF LOW
+#define SENSOR_COMP_VALUE 500
+#define BLOCK_SIGNAL_REC_VALUE 300
+#define BLOCK_SIGNAL_SEND_VALUE 1023
+
+// to enable and display the console data print
+const bool displayOn = true;
+
+int sen1_A1 = -1;
+int sen2_A2 = -1;
+int prevBlockSignal_A3 = -1;
+int nxtBlockSignal_A4  = -1;
+
+
+enum SIGNAL_STATES {
+  ST_G1_G2,
+  ST_G1_Y2,
+  ST_Y1_G2,
+  ST_Y1_Y2,
+  ST_R1_R2_1,
+  ST_R1_R2_2,
+  ST_R1_R2_3,
+}; //Sets up different signal states for the entire program
+
+SIGNAL_STATES signalState = ST_G1_G2; //Sets default state to Green and Green
+
+Auto485 bus(DE_PIN);
+CMRI cmri(CMRI_ADDR, 24, 48, bus);
+
+void setup() {
+
+  bus.begin(9600);
+  
+  Serial.begin(9600);
+
+  //Establish all of our pins at inputs or outputs
+
+  pinMode(sens1, INPUT);
+  pinMode(sens2, INPUT);
+  pinMode(G1_LED, OUTPUT);
+  pinMode(R1_LED, OUTPUT);
+  pinMode(G2_LED, OUTPUT);
+  pinMode(R2_LED, OUTPUT);
+  pinMode(blockOccSignal, OUTPUT);
+  pinMode(prevBlockRec, INPUT);
+  pinMode(nxtBlockRec, INPUT);
+
+}
+
+void loop() {
+
+  cmri.process();
+
+  sen1_A1 = analogRead(sens1);
+  sen2_A2 = analogRead(sens2);
+
+  prevBlockSignal_A3 = analogRead(prevBlockRec);
+  nxtBlockSignal_A4 = analogRead(nxtBlockRec);
+  // Sets up the values of the different signals we will be reading
+
+  if (displayOn) {
+    display();
+  }
+
+  // Sets up the different values to be read on the serial monitor in the Arduino IDE
+  delay(SENSOR_COMP_VALUE);
+
+  switch (signalState) {
+    case ST_G1_G2:
+      signal_G1_G2(sen1_A1, sen2_A2, prevBlockSignal_A3, nxtBlockSignal_A4);
+      break;
+    case ST_G1_Y2:
+      signal_G1_Y2(sen1_A1, sen2_A2, prevBlockSignal_A3, nxtBlockSignal_A4);
+      break;
+    case ST_Y1_G2:
+      signal_Y1_G2(sen1_A1, sen2_A2, prevBlockSignal_A3, nxtBlockSignal_A4);
+      break;
+    case ST_Y1_Y2:
+      signal_Y1_Y2(sen1_A1, sen2_A2, prevBlockSignal_A3, nxtBlockSignal_A4);
+      break;
+    case ST_R1_R2_1:
+      signal1_R1_R2_(sen1_A1, sen2_A2);
+      break;
+    case ST_R1_R2_2:
+      signal2_R1_R2_(sen1_A1, sen2_A2);
+      break;
+    case ST_R1_R2_3:
+      signal3_R1_R2_(sen1_A1, sen2_A2);
+      break;
+      //this sets up our different loops within the main loop for our different signal states
+  }
+}
+
+
+void signal_G1_G2(int sen1_A1, int sen2_A2,
+                  int prevBlockSignal_A3, int nxtBlockSignal_A4) {
+
+  Serial.println("signal_G1_G2");
+
+  GREEN_1_ON(); //Sets G1_LED to ON
+  GREEN_2_ON(); //Sets G2_LED to ON
+
+  analogWrite(blockOccSignal, 0); //this will not transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)
+      && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+      && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_Y2;//block not occupied but Block B is
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_G2;//block not occupied but Block A is
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_Y2;//block not occupied but Block B and Block A are
+  } else if ((sen1_A1 < SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_1;// block is occupied entered from sens1
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 < SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_2;// block is occupied entered from sens2
+  }
+}
+
+void signal_G1_Y2(int sen1_A1, int sen2_A2,
+                  int prevBlockSignal_A3, int nxtBlockSignal_A4) {
+
+  Serial.println("signal_G1_Y2");
+
+  GREEN_1_ON(); //Sets G1_LED to ON
+  YELLOW_2_ON();//Sets Y2_LED to ON
+
+  analogWrite(blockOccSignal, 0); //this will not transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)
+      && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+      && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_G2;//block not occupied No adjacent blocks occupied
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_G2;//block not occupied but Block A is
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_Y2;//block not occupied but Block B and Block A are
+  } else if ((sen1_A1 < SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_1;// block is occupied entered from sens1
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 < SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_2;// block is occupied entered from sens2
+  }
+}
+
+void signal_Y1_G2(int sen1_A1, int sen2_A2,
+                  int prevBlockSignal_A3, int nxtBlockSignal_A4) {
+
+  Serial.println("signal_Y1_G2");
+
+  YELLOW_1_ON(); //Sets Y1_LED to ON
+  GREEN_2_ON(); //Sets G2_LED to ON
+
+  analogWrite(blockOccSignal, 0); //this will not transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)
+      && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+      && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_G2;//block not occupied No adjacent blocks occupied
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_Y2;//block not occupied but Block B is
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_Y2;//block not occupied but Block B and Block A are
+  } else if ((sen1_A1 < SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_1;// block is occupied entered from sens1
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 < SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_2;// block is occupied entered from sens2
+  }
+}
+
+void signal_Y1_Y2(int sen1_A1, int sen2_A2,
+                  int prevBlockSignal_A3, int nxtBlockSignal_A4) {
+
+  Serial.println("signal_Y1_Y2");
+
+  YELLOW_1_ON();//Sets Y1_LED to ON
+  YELLOW_2_ON();//Sets Y2_LED to ON
+
+  analogWrite(blockOccSignal, 0); //this will not transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)
+      && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+      && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_G2;//block not occupied No adjacent blocks occupied
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 < BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 > BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_G1_Y2;//block not occupied but Block B is
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)
+             && (prevBlockSignal_A3 > BLOCK_SIGNAL_REC_VALUE)
+             && (nxtBlockSignal_A4 < BLOCK_SIGNAL_REC_VALUE)) {
+    signalState = ST_Y1_G2;//block not occupied but Block B and Block A are
+  } else if ((sen1_A1 < SENSOR_COMP_VALUE)
+             && (sen2_A2 > SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_1;// block is occupied entered from sens1
+  } else if ((sen1_A1 > SENSOR_COMP_VALUE)
+             && (sen2_A2 < SENSOR_COMP_VALUE)) {
+    signalState = ST_R1_R2_2;// block is occupied entered from sens2
+  }
+}
+
+void signal1_R1_R2_(int sen1_A1, int sen2_A2) {
+
+  Serial.println("signal1_R1_R2_");
+
+  RED_1_ON();//Sets R1_LED to ON
+  RED_2_ON();//Sets R2_LED to ON
+
+  analogWrite(blockOccSignal, BLOCK_SIGNAL_SEND_VALUE); //this will transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE) && (sen2_A2 < SENSOR_COMP_VALUE)) {
+    // setting sensor 0 with 1 active
+    cmri.set_bit(0, 1);
+    signalState = ST_R1_R2_3; //switched to transition Red signal state
+  }
+}
+
+void signal2_R1_R2_(int sen1_A1, int sen2_A2) {
+
+  Serial.println("signal2_R1_R2_");
+
+  RED_1_ON();//Sets R1_LED to ON
+  RED_2_ON();//Sets R2_LED to ON
+
+  analogWrite(blockOccSignal, BLOCK_SIGNAL_SEND_VALUE); //this will transmit a signal to Block A and B
+  delay(SENSOR_COMP_VALUE);
+
+  if ((sen1_A1 < SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)) {
+    // setting sensor 0 with 1 active
+    cmri.set_bit(0, 1);
+    signalState = ST_R1_R2_3; //switched to transition Red signal state
+  }
+}
+
+
+void signal3_R1_R2_(int sen1_A1, int sen2_A2) {
+
+  Serial.println("signal3_R1_R2_");
+
+  RED_1_ON();//Sets R1_LED to ON
+  RED_2_ON();//Sets R2_LED to ON
+
+  analogWrite(blockOccSignal, BLOCK_SIGNAL_SEND_VALUE); //this will transmit a signal to Block A and B
+  delay(DELAY_VALUE);
+
+  if ((sen1_A1 > SENSOR_COMP_VALUE)
+      && (sen2_A2 > SENSOR_COMP_VALUE)) {
+
+    // setting sensor 0 with 0 in-active
+    cmri.set_bit(0, 0);
+    signalState = ST_G1_G2; //switched to Green unoccupied signal
+  }
+}
+
+void RED_1_ON() {
+  digitalWrite(G1_LED, OFF);
+  digitalWrite(R1_LED, ON);
+}
+
+void RED_2_ON() {
+  digitalWrite(G2_LED, OFF);
+  digitalWrite(R2_LED, ON);
+}
+
+void YELLOW_1_ON() {
+  digitalWrite(G1_LED, ON);
+  digitalWrite(R1_LED, ON);
+}
+
+void YELLOW_2_ON() {
+  digitalWrite(G2_LED, ON);
+  digitalWrite(R2_LED, ON);
+}
+
+void GREEN_1_ON() {
+  digitalWrite(G1_LED, ON);
+  digitalWrite(R1_LED, OFF);
+}
+
+void GREEN_2_ON() {
+  digitalWrite(G2_LED, ON);
+  digitalWrite(R2_LED, OFF);
+}
+
+void display( ) {
+  Serial.println(sen1_A1);
+  Serial.println(sen1_A1);
+  Serial.println(prevBlockSignal_A3);
+  Serial.println(nxtBlockSignal_A4);
+}
