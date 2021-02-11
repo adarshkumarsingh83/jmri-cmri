24a473ddce4256ac25029d151eb929837e887a87
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-02-11 10:56:17 -0600

    cmri-ir-sensor-multi-block-system

--- APPLICATIONS/cmri-ir-sensor-multi-block-system/cmri-ir-sensor-multi-block-system.ino
+++ APPLICATIONS/cmri-ir-sensor-multi-block-system/cmri-ir-sensor-multi-block-system.ino
@@ -7,25 +7,26 @@
 #define OFF HIGH
 #define DELAY_VALUE 300
 
+enum SIGNAL_STATES {
+  UNOCCUPIED,
+  OCCUPIED_FROM_START_OF_BLOCK,
+  OCCUPIED_FROM_END_OF_BLOCK,
+  OCCUPIED_BLOCK,
+};
+
+
+const int SENSOR_COUNT = 3;
+SIGNAL_STATES sensorsState[SENSOR_COUNT] = {UNOCCUPIED, UNOCCUPIED, UNOCCUPIED};
+SIGNAL_STATES stateCurrent = UNOCCUPIED;
+SIGNAL_STATES statePrevious = UNOCCUPIED;
+const bool ENABLE_CONSOLE = false;
+
+
 int r;
 int c;
-int startSignal;
-int endSignal;
-const int SENSOR_COUNT = 3;
-int blockSignalSensorVal[SENSOR_COUNT][2];
-int blockSignalSensor[SENSOR_COUNT][2] = {{3, 4}, {5, 6}, {7, 8}};
+int blockSensorVal[SENSOR_COUNT][2];
+int blockSensor[SENSOR_COUNT][2] = {{3, 4}, {5, 6}, {7, 8}};
 
-bool ENABLE_CONSOLE = false;
-
-enum SIGNAL_STATES {
-  ST_G1_G2,
-  ST_R1_R2_1,
-  ST_R1_R2_2,
-  ST_R1_R2_3,
-};
-SIGNAL_STATES sensorsSignalState[SENSOR_COUNT] = {ST_G1_G2, ST_G1_G2, ST_G1_G2};
-SIGNAL_STATES signalStateCurrent = ST_G1_G2;
-SIGNAL_STATES signalStatePrevious = ST_G1_G2;
 
 Auto485 bus(DE_PIN);
 CMRI cmri(CMRI_ADDR, 24, 48, bus);
@@ -40,10 +41,11 @@ void setup() {
 
   for (r = 0; r < SENSOR_COUNT; r++) {
     for (c = 0; c < 2; c++) {
-      pinMode(blockSignalSensor[r][c], INPUT);
+      pinMode(blockSensor[r][c], INPUT);
     }
   }
 }
+
 
 void loop() {
 
@@ -51,52 +53,54 @@ void loop() {
 
   for (r = 0; r < SENSOR_COUNT; r++) {
     for (c = 0; c < 2; c++) {
-      blockSignalSensorVal[r][c] = digitalRead(blockSignalSensor[r][c]);
+      blockSensorVal[r][c] = digitalRead(blockSensor[r][c]);
     }
   }
 
   delay(DELAY_VALUE);
 
   for (r = 0 ; r < SENSOR_COUNT; r++) {
-    signalStatePrevious = sensorsSignalState[r];
-    switch (signalStatePrevious) {
-      case ST_G1_G2:
-        signalStateCurrent =  signal_G1_G2(r, signalStatePrevious, blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
-        if (signalStateCurrent != signalStatePrevious) {
-          setCmriSingals(r, signalStateCurrent);
-          sensorsSignalState[r] = signalStateCurrent;
+    statePrevious = sensorsState[r];
+    switch (statePrevious) {
+      case UNOCCUPIED:
+        stateCurrent =  unOccupiedBlock(statePrevious, blockSensorVal[r][0], blockSensorVal[r][1]);
+        if (stateCurrent != statePrevious) {
+          setCmriSingals(r, stateCurrent);
+          sensorsState[r] = stateCurrent;
         }
-        displayData(r, "signal_G1_G2", blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
+        displayData(r, "unOccupiedBlock", blockSensorVal[r][0], blockSensorVal[r][1]);
         break;
-      case ST_R1_R2_1:
-        signalStateCurrent = signal1_R1_R2(r, signalStatePrevious, blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
-        if (signalStateCurrent != signalStatePrevious) {
-          setCmriSingals(r, signalStateCurrent);
-          sensorsSignalState[r] = signalStateCurrent;
+      case OCCUPIED_FROM_START_OF_BLOCK:
+        stateCurrent = occupiedFromEndOfBlock(statePrevious, blockSensorVal[r][0], blockSensorVal[r][1]);
+        if (stateCurrent != statePrevious) {
+          setCmriSingals(r, stateCurrent);
+          sensorsState[r] = stateCurrent;
         }
-        displayData(r, "signal1_R1_R2", blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
+        displayData(r, "occupiedFromEndOfBlock", blockSensorVal[r][0], blockSensorVal[r][1]);
         break;
-      case ST_R1_R2_2:
-        signalStateCurrent = signal2_R1_R2(r, signalStatePrevious, blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
-        if (signalStateCurrent != signalStatePrevious) {
-          setCmriSingals(r, signalStateCurrent);
-          sensorsSignalState[r] = signalStateCurrent;
+      case OCCUPIED_FROM_END_OF_BLOCK:
+        stateCurrent = occupiedFromStartOfBlock(statePrevious, blockSensorVal[r][0], blockSensorVal[r][1]);
+        if (stateCurrent != statePrevious) {
+          setCmriSingals(r, stateCurrent);
+          sensorsState[r] = stateCurrent;
         }
-        displayData(r, "signal2_R1_R2", blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
+        displayData(r, "occupiedFromStartOfBlock", blockSensorVal[r][0], blockSensorVal[r][1]);
         break;
-      case ST_R1_R2_3:
-        signalStateCurrent = signal3_R1_R2(r, signalStatePrevious, blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
-        if (signalStateCurrent != signalStatePrevious) {
-          setCmriSingals(r, signalStateCurrent);
-          sensorsSignalState[r] = signalStateCurrent;
+      case OCCUPIED_BLOCK:
+        stateCurrent = occupiedBlock(statePrevious, blockSensorVal[r][0], blockSensorVal[r][1]);
+        if (stateCurrent != statePrevious) {
+          setCmriSingals(r, stateCurrent);
+          sensorsState[r] = stateCurrent;
         }
-        displayData(r, "signal3_R1_R2", blockSignalSensorVal[r][0], blockSignalSensorVal[r][1]);
+        displayData(r, "occupiedBlock", blockSensorVal[r][0], blockSensorVal[r][1]);
         break;
     }
   }
 }
+
+
 void setCmriSingals(int sensorNo, SIGNAL_STATES signalState) {
-  if (signalState == ST_G1_G2) {
+  if (signalState == UNOCCUPIED) {
     // setting sensor 0 with 0 in-active
     cmri.set_bit(sensorNo, 0);
   } else {
@@ -105,41 +109,44 @@ void setCmriSingals(int sensorNo, SIGNAL_STATES signalState) {
   }
 }
 
-SIGNAL_STATES signal_G1_G2(int sensorNo, SIGNAL_STATES signalState, int startSensor, int endSensor) {
+
+SIGNAL_STATES unOccupiedBlock(SIGNAL_STATES signalState, int startSensor, int endSensor) {
 
   if (startSensor == ON  && endSensor == OFF) {
-    signalState = ST_R1_R2_1;// block is occupied entered from sens1
+    signalState = OCCUPIED_FROM_START_OF_BLOCK;// block is occupied entered from sens1
   } else if (startSensor == OFF  && endSensor == ON) {
-    signalState = ST_R1_R2_2;// block is occupied entered from sens2
+    signalState = OCCUPIED_FROM_END_OF_BLOCK;// block is occupied entered from sens2
   }
   return signalState;
 }
 
 
-SIGNAL_STATES signal1_R1_R2(int sensorNo, SIGNAL_STATES signalState, int startSensor, int endSensor) {
+SIGNAL_STATES occupiedFromEndOfBlock(SIGNAL_STATES signalState, int startSensor, int endSensor) {
 
   if (startSensor == OFF && endSensor == ON) {
-    signalState = ST_R1_R2_3; //switched to transition Red signal state
+    signalState = OCCUPIED_BLOCK; //switched to transition Red signal state
   }
   return signalState;
 }
 
-SIGNAL_STATES signal2_R1_R2(int sensorNo, SIGNAL_STATES signalState, int startSensor, int endSensor) {
+
+SIGNAL_STATES occupiedFromStartOfBlock(SIGNAL_STATES signalState, int startSensor, int endSensor) {
 
   if (startSensor == ON  && endSensor == OFF) {
-    signalState = ST_R1_R2_3; //switched to transition Red signal state
+    signalState = OCCUPIED_BLOCK; //switched to transition Red signal state
   }
   return signalState;
 }
 
 
-SIGNAL_STATES signal3_R1_R2(int sensorNo, SIGNAL_STATES signalState, int startSensor, int endSensor) {
+SIGNAL_STATES occupiedBlock(SIGNAL_STATES signalState, int startSensor, int endSensor) {
 
   if ((startSensor == OFF) && (endSensor == OFF)) {
-    signalState = ST_G1_G2; //switched to Green unoccupied signal
+    signalState = UNOCCUPIED; //switched to Green unoccupied signal
   }
   return signalState;
 }
+
 
 void displayData(int sensorNo, String message, int startSensor, int endSensor) {
   if (ENABLE_CONSOLE) {
