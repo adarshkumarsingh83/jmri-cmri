84a7f0259c2909637a0fe244b89c09f9a63fa00c
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-05-01 15:58:31 -0500

    adding Nmra-Dcc-Accssory-Decoder-Basic

--- APPLICATIONS/Nmra-Dcc-Accssory-Decoder-Basic/Nmra-Dcc-Accssory-Decoder-Basic.ino
+++ APPLICATIONS/Nmra-Dcc-Accssory-Decoder-Basic/Nmra-Dcc-Accssory-Decoder-Basic.ino
@@ -6,18 +6,27 @@
 
 NmraDcc Dcc;
 
+void notifyDccSigOutputState( uint16_t Addr, uint8_t State) {
+  Serial.println(" LIGHT OR SIGNAL CONTROL");
+  Serial.print("Addr: "); Serial.println(Addr);
+  Serial.print("State: "); Serial.println(State);
+  Serial.println("--------------------");
+}
+
+
 void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower) {
 
-  if(Addr == 1) {
-    if(Direction == 0) {
+  if (Addr == 1) {
+    if (Direction == 0) {
       digitalWrite(RED_LED, HIGH);
       digitalWrite(GREEN_LED, LOW);
     } else {
       digitalWrite(RED_LED, LOW);
-      digitalWrite(GREEN_LED, HIGH);    
+      digitalWrite(GREEN_LED, HIGH);
     }
   }
-    
+  
+  Serial.println(" TURNOUT CONTROL");
   Serial.print("Addr: "); Serial.println(Addr);
   Serial.print("Direction: "); Serial.println(Direction);
   Serial.print("OutputPower: "); Serial.println(OutputPower);
@@ -28,7 +37,7 @@ void setup() {
 
   Serial.begin(115200);
   pinMode(RED_LED, OUTPUT);
-  pinMode(GREEN_LED, OUTPUT); 
+  pinMode(GREEN_LED, OUTPUT);
   Dcc.pin(digitalPinToInterrupt(DCC_PIN), DCC_PIN, false);
   Dcc.init(MAN_ID_DIY, 1, FLAGS_DCC_ACCESSORY_DECODER | FLAGS_OUTPUT_ADDRESS_MODE, 0);
 }
