9c038834b869215b17ae39ee49ec74b2881ae7a6
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-07-30 10:59:53 -0500

    adding config

---
+++ DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-esp8266-uno-node-string-serial/arduino_uno_slave/Config.h
@@ -0,0 +1,16 @@
+#ifndef Config_h
+#define Config_h
+#include "Arduino.h"
+
+#define THROWN "TH"
+#define CLOSED "CL"
+#define ON "ON"
+#define OFF "OF"
+
+#define LIGHT_START_ADDRESS 1000
+#define TURNOUT_START_ADDRESS 2000
+#define SIGNAL_START_ADDRESS 2000
+
+#define CACHE_LIMIT 20
+
+#endif
--- DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-esp8266-uno-node-string-serial/arduino_uno_slave/arduino_uno_slave.ino
+++ DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-esp8266-uno-node-string-serial/arduino_uno_slave/arduino_uno_slave.ino
@@ -1,11 +1,7 @@
 #include "CircularQueue.cpp"
+#include"Config.h"
 
-#define THROWN "TH"
-#define CLOSED "CL"
-#define ON "ON"
-#define OFF "OF"
-
-const CircularQueue<String> queue = CircularQueue<String>(20, "mqtt-msg");
+const CircularQueue<String> queue = CircularQueue<String>(CACHE_LIMIT, "mqtt-msg");
 
 void processCall(String message) {
   Serial.println(message);
@@ -17,21 +13,21 @@ void processCall(String message) {
 
   int id = atoi(number.c_str());
 
-  if (id <= 2000) { //all the light are started with 1000 onwards to 1999 address on jmri
+  if (id <= LIGHT_START_ADDRESS) { //all the light are started with 1000 onwards to 1999 address on jmri
     if (value == ON) {
       Serial.println("Light Number " + number + "  " + value);
     } else if (value == OFF) {
       Serial.println("Light Number " + number + "  " + value);
     }
 
-  } else if (id >= 2000 && id < 3000) { //all the turnout are started with 2000 onwards to 2999 address on jmri
+  } else if (id >= TURNOUT_START_ADDRESS && id < SIGNAL_START_ADDRESS) { //all the turnout are started with 2000 onwards to 2999 address on jmri
     if (value == THROWN) {
       Serial.println("Turnout Number " + number + "  " + value);
     } else if (value == CLOSED) {
       Serial.println("Turnout Number " + number + "  " + value);
     }
 
-  } else if (id >= 3000) { // all the signal are started with 3000 onwards address on jmri
+  } else if (id >= SIGNAL_START_ADDRESS) { // all the signal are started with 3000 onwards address on jmri
     if (value == THROWN) {
       Serial.println("Signal Number " + number + " ON");
     } else if (value == CLOSED) {
