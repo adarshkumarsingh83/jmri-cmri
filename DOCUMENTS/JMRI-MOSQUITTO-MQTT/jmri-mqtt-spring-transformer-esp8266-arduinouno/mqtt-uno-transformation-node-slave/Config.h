fd7ec2f0ee053140615be69bf48bfb5ed1b82d47
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-07-30 11:05:18 -0500

    adding config

---
+++ DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-spring-transformer-esp8266-arduinouno/mqtt-uno-transformation-node-slave/Config.h
@@ -0,0 +1,14 @@
+#ifndef Config_h
+#define Config_h
+#include "Arduino.h"
+
+#define THROWN "TH"
+#define CLOSED "CL"
+#define ON "ON"
+#define OFF "OF"
+
+#define LIGHT_START_ADDRESS 1000
+#define TURNOUT_START_ADDRESS 2000
+#define SIGNAL_START_ADDRESS 2000
+
+#endif
--- DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-spring-transformer-esp8266-arduinouno/mqtt-uno-transformation-node-slave/mqtt-uno-transformation-node-slave.ino
+++ DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-spring-transformer-esp8266-arduinouno/mqtt-uno-transformation-node-slave/mqtt-uno-transformation-node-slave.ino
@@ -1,9 +1,4 @@
-
-#define THROWN "TH"
-#define CLOSED "CL"
-#define ON "ON"
-#define OFF "OF"
-
+#include "Config.h"
 
 void processCall(String message) {
   Serial.println(message);
@@ -15,21 +10,21 @@ void processCall(String message) {
 
   int id = atoi(number.c_str());
 
-  if (id <= 2000) { //all the light are started with 1000 onwards to 1999 address on jmri
+  if (id >= LIGHT_START_ADDRESS && id <= TURNOUT_START_ADDRESS) { //all the light are started with 1000 onwards to 1999 address on jmri
     if (value == ON) {
       Serial.println("Light Number " + number + "  " + value);
     } else if (value == OFF) {
       Serial.println("Light Number " + number + "  " + value);
     }
 
-  } else if (id >= 2000 && id < 3000) { //all the turnout are started with 2000 onwards to 2999 address on jmri
+  } else if (id >= TURNOUT_START_ADDRESS && id < SIGNAL_START_ADDRESS) { //all the turnout are started with 2000 onwards to 2999 address on jmri
     if (value == THROWN) {
       Serial.println("Turnout Number " + number + "  " + value);
     } else if (value == CLOSED) {
       Serial.println("Turnout Number " + number + "  " + value);
     }
 
-  } else if (id >= 3000) { // all the signal are started with 3000 onwards address on jmri
+  } else if (id >= SIGNAL_START_ADDRESS) { // all the signal are started with 3000 onwards address on jmri
     if (value == THROWN) {
       Serial.println("Signal Number " + number + " ON");
     } else if (value == CLOSED) {
@@ -43,7 +38,6 @@ void processCall(String message) {
 void setup() {
   Serial.begin(115200);
 }
-
 
 void loop() {
   // Monitor serial communication
