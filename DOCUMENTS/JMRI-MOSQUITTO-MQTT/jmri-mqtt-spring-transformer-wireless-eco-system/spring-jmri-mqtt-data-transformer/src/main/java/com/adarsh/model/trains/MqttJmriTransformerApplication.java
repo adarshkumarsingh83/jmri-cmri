632cf775f488206a185be4f1879d1905d1d1aab4
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-08-19 19:23:08 -0500

    jmri-mqtt-spring-transformer-wireless-eco-system

--- DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-spring-transformer-wireless-eco-system/spring-jmri-mqtt-data-transformer/src/main/java/com/adarsh/model/trains/MqttJmriTransformerApplication.java
+++ DOCUMENTS/JMRI-MOSQUITTO-MQTT/jmri-mqtt-spring-transformer-wireless-eco-system/spring-jmri-mqtt-data-transformer/src/main/java/com/adarsh/model/trains/MqttJmriTransformerApplication.java
@@ -1,8 +1,18 @@
 package com.adarsh.model.trains;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /*
  * @author Adarsh
@@ -10,12 +20,31 @@ import org.springframework.integration.annotation.IntegrationComponentScan;
  * @version $Revision: 0001 $, $Date:: 15/6/20 10:12 AM#$
  */
 
+@Slf4j
 @IntegrationComponentScan
 @SpringBootApplication
 public class MqttJmriTransformerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MqttJmriTransformerApplication.class, args);
+        // SpringApplication.run(MqttJmriTransformerApplication.class, args);
+        SpringApplication application = new SpringApplication(MqttJmriTransformerApplication.class);
+        application.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                String banner = readBanner(); // REPLACE WITH ANY STRING
+                out.print(banner);
+            }
+        });
+        application.run(args);
     }
 
+    private static String readBanner() {
+        try {
+            String banner = new String(Files.readAllBytes(Paths.get(ResourceUtils.getFile("classpath:amt-banner.txt").toURI())), StandardCharsets.UTF_8);
+            return banner;
+
+        } catch (IOException ex) {
+            return "Banner not found";
+        }
+    }
 }
